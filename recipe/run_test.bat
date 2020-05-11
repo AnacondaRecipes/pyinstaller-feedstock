58cc6ec479e76efa19d1e19d87d8239d07b1bddb
Ray Donnelly <mingw.android@gmail.com>
2020-05-11 12:30:40 +0200

    Add multiprocessing_test.py

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,5 +1,9 @@
 pyinstaller -n hello hello.py
-
 dir dist\hello
 dist\hello\hello.exe
+if errorlevel 1 exit 1
+
+pyinstaller -n multiprocessing_test multiprocessing_test.py
+dir dist\multiprocessing_test
+dist\multiprocessing_test\multirpocessing_test.exe
 if errorlevel 1 exit 1
