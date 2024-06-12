f18ac45b7a460d4f3dc36c07f3d87f6389bc9857
anaobi <anaobii@gmail.com>
2024-06-12 15:46:06 +0100

    update to v6.8.0

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,34 +1,7 @@
-#!/usr/bin/env bash
+# Remove the test directory which import tkinter which requires X11
+rm -rf $SP_DIR/../test
 
-pyinstaller --help
-pyi-archive_viewer --help
-pyi-bindepend --help
-pyi-makespec --help
+pyinstaller -n hello hello.py
 
-pip check
-
-declare -a _RUN_DEBUG=()
-# If you need to figure out what's going on here:
-# _RUN_DEBUG+=('-d')
-# all,imports,bootloader,noarchive
-# _RUN_DEBUG+=('-all')
-
-declare -a _BUILD_DEBUG=()
-_BUILD_DEBUG+=('--log-level')
-_BUILD_DEBUG+=('INFO')
-
-declare -a _PROJECTS=()
-_PROJECTS+=('hello')
-_PROJECTS+=('multiprocessing_test')
-
-for _PROJECT in "${_PROJECTS[@]}"; do
-  echo "Checking we can build ${_PROJECT}"
-  [[ -d "${HOME}/Library/Application Support/pyinstaller" ]] && rm -rf "${HOME}/Library/Application Support/pyinstaller"
-  _DISTPATH=${PWD}/dist.${PYVER}.${_PROJECT}
-  _WORKPATH=${PWD}/work.${PYVER}.${_PROJECT}
-  [[ -d ${_DISTPATH} ]] && rm -rf ${_DISTPATH}
-  [[ -d ${_WORKPATH} ]] && rm -rf ${_WORKPATH}
-  pyinstaller "${_RUN_DEBUG[@]}" "${_BUILD_DEBUG[@]}" --distpath=${_DISTPATH} --workpath=${_WORKPATH} --noconfirm -n ${_PROJECT} ${_PROJECT}.py
-  ls -lh ${_DISTPATH}/${_PROJECT}
-  ${_DISTPATH}/${_PROJECT}/./${_PROJECT}
-done
+ls -lh dist/hello
+dist/hello/./hello
