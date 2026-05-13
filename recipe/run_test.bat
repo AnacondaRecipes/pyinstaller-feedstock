1686518c6a7679761f1b7710e0831e7f4392d87a
Charles Bousseau <cbousseau@anaconda.com>
2026-05-13 15:27:46 -0400

    update tests

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -8,11 +8,7 @@ pyi-set_version --help
 pip check
 
 pyinstaller -n hello hello.py
+
 dir dist\hello
 dist\hello\hello.exe
-if errorlevel 1 exit 1
-
-pyinstaller -n multiprocessing_test multiprocessing_test.py
-dir dist\multiprocessing_test
-dist\multiprocessing_test\multiprocessing_test.exe
 if errorlevel 1 exit 1
